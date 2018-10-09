790da48891170d77eb2fc3eeab073fe134a61c11
pripisnov <pripisnov@bk.ru>
2018-10-10 01:47:33 +0300

    GUI->pre->mod->db

--- model/model.py
+++ model/model.py
@@ -1,6 +1,6 @@
 import json
 from abc import ABCMeta, abstractmethod
-import conn_db
+import dba.conn_db as conn_db
 
 
 class AbsModel:
@@ -54,6 +54,8 @@ class SQLiteRepository(AbsModel):
         super().__init__(file_name, data)
         self.db_ = conn_db.CDataBase()
         self.project_name = None
+        self.project_id = None
+        self.projects_names = None
 
     def create_project(self, **kwargs):
         print(kwargs)
@@ -61,23 +63,37 @@ class SQLiteRepository(AbsModel):
         self.db_.set_project(prj_name=kwargs['project_name'],
                              author=kwargs['author_name'],
                              link_original=kwargs['source_link'])
+        if self.get_project_id() is not None:
+            self.project_id = self.get_project_id()
 
     def open_project(self, project_name: str):
+        print('hi, im open project')
         self.project_name = project_name
+        self.project_id = self.get_project_id()
         return self.get_data()
 
+    def get_project_id(self):
+        print('hi, im get project id')
+        return self.db_.get_project_id(self.project_name)
+
+    def get_projects_names(self):
+        print('im here!')
+        self.projects_names = self.db_.get_projects_names()
+        return tuple(str(i[0]) for i in self.projects_names)
+
     def get_data(self):
-        en_ = self.db_.get_few_block_en(self.project_name)
-        ru_ = self.db_.get_few_block_ru(self.project_name)
-        return tuple((en_[i], ru_[i]) for i in en_)
+        print('hi im get data for id:', self.project_id)
+        blocks = self.db_.get_all_block(self.project_id)
+        if blocks:
+            return tuple((i[0], i[1]) for i in blocks)
 
     def set_data(self, data: tuple):
         print(data)
         for idx, i in enumerate(data):
             print('en', i[0])
             print('ru', i[1])
-            self.db_.set_en_text(prj_name=self.project_name, block_id=idx, en_text=i[0])
-            self.db_.set_ru_text(prj_name=self.project_name, block_id=idx, ru_text=i[1])
+            self.db_.set_en_text(prj_id=self.project_id, block_id=idx, en_text=i[0])
+            self.db_.set_ru_text(prj_id=self.project_id, block_id=idx, ru_text=i[1])
 
 
 class TempRepository(AbsModel):
