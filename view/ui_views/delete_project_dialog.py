04ad1b9ffed7a98840c8d1f8911046e6cee90964
pripisnov <pripisnov@bk.ru>
2018-10-14 00:26:24 +0300

    gui del methods

---
+++ view/ui_views/delete_project_dialog.py
@@ -0,0 +1,25 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
+
+from view.ui_views.deleteProjectWindow import Ui_Dialog
+
+
+class DeleteProjectDialogWindow(QtWidgets.QDialog, Ui_Dialog):
+
+    def __init__(self, parent=None):
+        QtWidgets.QDialog.__init__(self, parent)
+        self.setupUi(self)
+        self.cancelPushButton.clicked.connect(self.close)
+        self.deletePushButton.clicked.connect(self.delete_project_clicked)
+        self.deleteProjectListWidget.itemDoubleClicked.connect(self.delete_project_clicked)
+
+    def delete_project_clicked(self):
+        project_item = self.deleteProjectListWidget.currentItem()
+        if project_item:
+            project_name = project_item.text()
+            self.parent().delete_project.emit(project_name)
+            self.close()
+        else:
+            self.parent().info_box('info', 'Не выбран проект', 'Выберите проект')
+
+    def add_projects_list(self, projects):
+        self.deleteProjectListWidget.addItems(projects)
